5bc03c2943d703535cf9448bae879afcc3d41faa
Zachary Shenkman <yankeedude02@gmail.com>
2023-04-05 11:47:17 -0400

    Adds Sentry

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const cors = require('cors')
+const Sentry = require('@sentry/node')
+const Tracing = require('@sentry/tracing')
 const https = require('https')
 const axios = require('axios').default
 const dayjs = require('dayjs')
@@ -29,7 +31,35 @@ const PARK_NAMES = {
 // Configures Express app
 const PORT = process.env.PORT || 8000
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(cors())
+
+// Configures Sentry
+const SENTRY_DSN = process.env.SENTRY_DSN
+Sentry.init({
+    dsn: SENTRY_DSN,
+    integrations: [
+        // enable HTTP calls tracing
+        new Sentry.Integrations.Http({ tracing: true }),
+        // enable Express.js middleware tracing
+        new Tracing.Integrations.Express({ app }),
+        // Automatically instrument Node.js libraries and frameworks
+        ...Sentry.autoDiscoverNodePerformanceMonitoringIntegrations()
+    ],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0
+})
+// RequestHandler creates a separate execution context using domains, so that every
+// transaction/span/breadcrumb is attached to its own Hub instance
+app.use(Sentry.Handlers.requestHandler())
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler())
+
+axios.defaults.headers.common['Accept-Encoding'] = '*'
 
 // Starts the express server
 app.listen(PORT, () => {
